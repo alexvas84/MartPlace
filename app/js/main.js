1525b8d8cdd98158f8b3f713f51e47e9c5f09066
alexvas84 <alexvas84@gmail.com>
2020-06-02 23:33:39 +0300

    add blog

--- app/js/main.js
+++ app/js/main.js
@@ -263,10 +263,10 @@ $(function () {
 	});
 
 
-	$('.product-one__tabs .tab').on('click', function (event) {
+	$('.product-one__tabs .tab, .blog-tabs .tab').on('click', function (event) {
 		var id = $(this).attr('data-id');
-		$('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
-		$('.product-one__tabs .tabs').find('.tab').removeClass('active');
+		$('.product-one__tabs, .blog-tabs').find('.tab-item').removeClass('active-tab').hide();
+		$('.product-one__tabs .tabs, .blog-tabs .tabs').find('.tab').removeClass('active');
 		$(this).addClass('active');
 		$('#' + id).addClass('active-tab').fadeIn();
 		return false;
